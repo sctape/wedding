13fd73f5b643a5462747e070b876e1badfa0517e
Sam Tape <sctape@gmail.com>
2014-09-02 00:47:14 -0500

    Reworking the form. Still need to add JS show/hide features.

--- app/views/rsvp/create.blade.php
+++ app/views/rsvp/create.blade.php
@@ -4,25 +4,56 @@
 
 <div class="row">
     <div class="col-md-6">
-        <div class="form-group">
-            {{ Form::label('name', 'Name') }}
-            {{ Form::text('name', null, ['class' => 'form-control']) }}
-        </div>
-        <div class="form-group">
-            {{ Form::label('plus_one', '+ 1') }}
-            {{ Form::text('plus_one', null, ['class' => 'form-control']) }}
-        </div>
-        <div class="form-group">
-            {{ Form::label('protein', 'Choose your protein') }}
-            <ul>
-                <li>{{ Form::checkbox('food[]', 1) }} {{ Form::label('beef', 'Beef') }}</li>
-                <li>{{ Form::checkbox('food[]', 1) }} {{ Form::label('chicken', 'Chicken') }}</li>
-                <li>{{ Form::checkbox('food[]', 1) }} {{ Form::label('pork', 'Pork') }}</li>
-                <li>{{ Form::checkbox('food[]', 1) }} {{ Form::label('vegetarian', 'Vegetarian') }}</li>
-            </ul>
-        </div>
+    <div class="form-group rsvp_button_container">
+    {{ ButtonGroup::withContents([
+                        Button::success('I gladly accept!'),
+                        Button::warning('I regretfully decline'),
+                      ]) }}
+    </div>
+
+
+{{ ControlGroup::generate(
+               Form::label('num_guests', 'Number of guests'),
+               Form::select('num_guests', [0, 1, 2, 3, 4, 5])
+           )->withAttributes(['class' => 'num_guests_container']) }}
+
+<div class="guest-sub-form">
+ {{ ControlGroup::generate(
+                   Form::label('name', 'Name'),
+                   Form::text('name')
+
+               ) }}
+
+        {{ ControlGroup::generate(
+               Form::label('control', 'Choose your protein!'),
+                       [
+                           [
+                               'label' => ['beef', 'Beef'],
+                               'input' => ['type' => 'checkbox', 'beef', 'beef']
+                           ],
+                           [
+                               'label' => ['chicken', 'Chicken'],
+                               'input' => ['type' => 'checkbox', 'chicken', 'chicken']
+                           ],
+                           [
+                               'label' => ['pork', 'Pork'],
+                               'input' => ['type' => 'checkbox', 'pork', 'pork']
+                           ],
+                           [
+                              'label' => ['vegetarian', 'vegetarian'],
+                              'input' => ['type' => 'checkbox', 'vegetarian', 'vegetarian']
+                           ],
+
+                       ],
+               null,
+               5
+               )->withAttributes(['class' => 'food']) }}
+</div>
+
     </div>
 
 </div>
+
+
 
 @stop
